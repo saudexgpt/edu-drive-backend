d825724f48e05a271876ccd7086489bd0145284a
sammy4best <saudexgpt@gmail.com>
2022-10-21 14:13:41 +0100

    Update3

--- app/Http/Controllers/SchoolsController.php
+++ app/Http/Controllers/SchoolsController.php
@@ -268,13 +268,13 @@ class SchoolsController extends Controller
         }
         $registered_school = $school->registerSchool($request, $user_id);
         if ($registered_school === 'Exist') {
-            return response()->json(["This School's information already exist"], 204);
+            return "This School's information already exist";
             # code...
         }
         // $request->school_id = $registered_school->id;
         // send mail to admin
 
-        return response()->json(["Registration Successful"], 200);
+        return "Registration Successful";
     }
     public function potentialSchools()
     {
