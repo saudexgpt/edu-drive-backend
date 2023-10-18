c4aaac08bd86b400255496eb11e01099005564a9
sammy4best <saudexgpt@gmail.com>
2023-10-18 13:27:26 +0100

    Update filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,8 +38,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => portalPulicPath(),
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
